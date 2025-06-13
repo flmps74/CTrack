49f9862c4e9a7f7b78babbfb44f7f8c8127f1e89
Parth Srivastava <parthsrivastava1415@gmail.com>
2025-06-12 20:58:09 -0500

    Added input fields for user data entry in the Cl equation form and implemented event listeners to calculate

---
+++ SmartAdvisor/advisor.js
@@ -0,0 +1,27 @@
+const goalDropdown = document.getElementById('goal');
+const formSection = document.getElementById('advisorForm');
+const outputBox = document.getElementById('output');
+const resultText = document.getElementById('resultText');
+
+goalDropdown.addEventListener("change", () => {
+    formSection.style.display = "block";
+    outputBox.style.display = "none";
+})
+
+formSection.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const initialAmount = parseFloat(document.getElementById("initial").value);
+    const monthlyAdd = parseFloat(document.getElementById("monthly").value);
+    const annualRate = parseFloat(document.getElementById("rate").value) / 100;
+    const years = parseFloat(document.getElementById("years").value);
+    const timesPerYear = 12; 
+
+    alert(initialAmount);
+    alert(Math.pow((1 + annualRate/timesPerYear), years * timesPerYear ));
+    const compoundPart = initialAmount * Math.pow(1 + annualRate/timesPerYear, timesPerYear * years);
+    alert(compoundPart);
+    
+    const monthlyPart = monthlyAdd * ((Math.pow(1 + annualRate/timesPerYear, timesPerYear * years) - 1) / (annualRate/timesPerYear));
+    alert(monthlyPart);
+})  
